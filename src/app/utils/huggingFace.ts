e158cbd86d22ed4fe68fc2501bef52a99f973856
Faith S.Appoh <appohfaiths@gmail.com>
2024-12-28 19:32:42 +0000

    remove token

--- src/app/utils/huggingFace.ts
+++ src/app/utils/huggingFace.ts
@@ -1,5 +1,5 @@
 import {HfInference} from "@huggingface/inference";
 
-const HF_TOKEN = "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"
+const HF_TOKEN = process.env.HF_TOKEN;
 
 export const inference = new HfInference(HF_TOKEN);
